80ac084e458da6b18932ad3ca942e825cf92154b
leelethal816 <zhangtianyi816@gmail.com>
2023-02-24 15:46:28 -0600

    Resetting the Game

--- settings.py
+++ settings.py
@@ -10,8 +10,8 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 5
-        self.ship_limit = 3
+        self.ship_speed = 10
+        self.ship_limit = 1
 
         # Bullet settings
         self.bullet_speed = 5.0
@@ -22,6 +22,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 30
         # fleet direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
