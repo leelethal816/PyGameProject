e322f403eb1766dde34dab2388482db6894b8cf2
leelethal816 <zhangtianyi816@gmail.com>
2023-02-25 11:20:49 -0600

    Displaying the Level

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -128,6 +129,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
         if collisions:
             for aliens in collisions.values():
--- scoreboard.py
+++ scoreboard.py
@@ -17,11 +17,12 @@ class Scoreboard:
         # Prepare the initial score images
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         # Turn the score into a rendered image
         rounded_score = round(self.stats.score, -1)
-        score_str = f"{rounded_score:,}"
+        score_str = f"Score: {rounded_score:,}"
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.settings.bg_color
         )
@@ -34,7 +35,7 @@ class Scoreboard:
     def prep_high_score(self):
         # Turn the high score into a rendered image
         high_score = round(self.stats.high_score, -1)
-        high_score_str = f"{high_score:,}"
+        high_score_str = f"High Score: {high_score:,}"
         self.high_score_image = self.font.render(
             high_score_str, True, self.text_color, self.settings.bg_color
         )
@@ -42,10 +43,23 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    def prep_level(self):
+        # Turn the level into a rendered image
+        level_str = str(f"Level {self.stats.level}")
+        self.level_image = self.font.render(
+            level_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # Position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.left = self.score_rect.left
+        self.level_rect.top = self.score_rect.bottom + 10
+
     def show_score(self):
-        # Draw the score to the screen
+        # Draw the score and level to the screen
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def check_high_score(self):
         # Check to see if there's a new high score
